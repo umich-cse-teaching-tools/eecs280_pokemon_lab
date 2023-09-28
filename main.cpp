57f73d4ebac8adf0373569564023c7ff4dcd7146
James Juett <jjuett@umich.edu>
2023-09-28 09:32:57 -0400

    add placeholder skeleton file

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <string>
+
+#include "Pokemon.hpp"
+#include "Trainer.hpp"
+
+using namespace std;
+
+class League {
+private:
+  // TODO: Add member variables here
+
+public:
+
+  // TODO: Add constructor here to initialize members
+  
+
+
+
+  // TODO: Add member functions here
+
+
+
+  
+
+  ~League() {
+    // TODO: put code here to clean up by deleting all Trainer objects
+    
+  }
+  
+};
+
+int main(int argc, char *argv[]) {
+
+  // TODO: Add code to read command line args and open file streams here
+  
+
+
+  // TODO: Create a League object, call function to run the simulation
+  
+
+  
+}
